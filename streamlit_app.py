462e44306b4093d08f171942920b2fab48dbb88f
ralph-vital <vitalralph@hotmail.com>
2024-08-23 22:57:36 -0400

    commit sarimax model

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import pandas as pd
 import math
 from pathlib import Path
 import plotly.express as px
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.graph_objects as go
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
@@ -25,11 +29,11 @@ def get_gdp_data():
     """
 
     # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/sdg_data_hti.csv'
-    gdp_df = pd.read_csv(DATA_FILENAME, skiprows=[1,])
+    DATA_FILENAME = Path(__file__).parent/'data/exchange-rates_hti.csv'
+    btc = pd.read_csv(DATA_FILENAME, skiprows=[1,])
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+    MIN_YEAR = 1970
+    MAX_YEAR = 2023
 
     # The data above has columns like:
     # - Country Name
@@ -55,74 +59,157 @@ def get_gdp_data():
     #     'GDP',
     # )
 
+    btc_x = btc[['StartDate', 'Value']]
+    btc_x = btc_x.sort_values('StartDate')
+    btc_x.index = pd.to_datetime(btc_x['StartDate'], format='%Y-%m-%d')
+    del btc_x['StartDate']
+
     # Convert years from string to integers
-    gdp_df['year'] = gdp_df['year'].astype(int)
 
-    return gdp_df
+    return btc_x
 
-
-gdp_df = get_gdp_data()
 
 # -----------------------------------------------------------------------------
 # Draw the actual page
-
 # Set the title that appears at the top of the page.
 '''
-# :earth_americas: Haiti Eduction Indicator dashboard
+# Forcasting Haiti Xchange Rate using SARIMEX
 
-Browse  data from the [Unesco Open Data](https://www.uil.unesco.org) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data. Happy visualisation 😊 
+
+SARIMAX and ARIMA forecasters
+SARIMAX (Seasonal Autoregressive Integrated Moving-Average with Exogenous Regressors) is a generalization of the ARIMA model that incorporates both seasonality and exogenous variables. SARIMAX models are among the most widely used statistical forecasting models with excellent forecasting performance.
+
+In the SARIMAX model notation, the parameters  
+p ,  d , and  q represent the autoregressive, differencing, and moving-average components, respectively.  
+P,  D, and  Q denote the same components for the seasonal part of the model, with  m representing the number of periods in each season.
+
+p: is the order (number of time lags) of the autoregressive part of the model.
+
+d: is the degree of differencing (the number of times that past values have been subtracted from the data).
+
+q: is the order of the moving-average part of the model.
+
+P: is the order (number of time lags) of the seasonal part of the model
+
+D: is the degree of differencing (the number of times the data have had past values subtracted) of the seasonal part of the model.
+
+Q: is the order of the moving-average of the seasonal part of the model.
+
+m: refers to the number of periods in each season.
 '''
 
 # Add some spacing
-''
-''
 
-min_value = gdp_df['year'].min()
-max_value = gdp_df['year'].max()
 
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
+@st.cache_data
+def get_y(btc_x):
+    train = btc_x[btc_x.index < pd.to_datetime(
+        "2021-12-31", format='%Y-%m-%d')]
+    test = btc_x[btc_x.index > pd.to_datetime(
+        "2021-12-31", format='%Y-%m-%d')]
+    return train, test
 
-countries = gdp_df['indicator_id'].unique()
 
-if not len(countries):
-    st.warning("Select at least one indicator")
+# get the data
+gdp_df = get_gdp_data()
+y, test = get_y(gdp_df)
 
-selected_countries = st.multiselect(
-    'Which indicators would you like to view?',
-    countries,
-    ['GER.5T8', 'GER.5T8.F', 'AIR.1.GLAST', 'AIR.1.GLAST.M', 'ROFST.1.CP', 'GER.5T8.M'])
+st.sidebar.header('SARIMA Parameters:')
+p = st.sidebar.select_slider(
+    "Select p",
+    options=[
+        0, 1, 2, 3, 4, 5, 6],
+    value=1
+)
 
-''
-''
-''
+d = st.sidebar.select_slider(
+    "Select d",
+    options=[
+        0, 1, 2, 3, 4, 5, 6],
+    value=0
+)
+
+q = st.sidebar.select_slider(
+    "Select q",
+    options=[
+        0, 1, 2, 3, 4, 5, 6],
+    value=1)
+P = st.sidebar.select_slider(
+    "Select P",
+    options=[
+        0, 1, 2, 3, 4, 5, 6],
+    value=0
+)
+D = st.sidebar.select_slider(
+    "Select D",
+    options=[
+        0, 1, 2, 3, 4, 5, 6],
+    value=0
+)
+Q = st.sidebar.select_slider(
+    "Select Q",
+    options=[
+        0, 1, 2, 3, 4, 5, 6],
+    value=0
+)
+
+m = st.sidebar.select_slider(
+    "Select m",
+    options=[
+        0, 1, 2, 3, 4, 5, 6],
+    value=0
+)
+
+
+def get_fit(SARIMAXmodel):
+    SARIMAXmodel = SARIMAXmodel.fit()
+    return SARIMAXmodel
+
+
+SARIMAXmodel = SARIMAX(y, order=(5, 4, 2), seasonal_order=(2, 2, 2, 12))
+with st.spinner('Wait for it...'):
+    SARIMAXmodel = get_fit(SARIMAXmodel)
+
+
+y_pred = SARIMAXmodel.get_forecast(len(test.index))
+y_pred_df = y_pred.conf_int(alpha=0.05)
+y_pred_df["Predictions"] = SARIMAXmodel.predict(
+    start=y_pred_df.index[0], end=y_pred_df.index[-1])
+y_pred_df.index = test.index
+y_pred_out = y_pred_df["Predictions"]
 
 # Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['indicator_id'].isin(selected_countries))
-    & (gdp_df['year'] <= to_year)
-    & (from_year <= gdp_df['year'])
-]
+# fig, ax = plt.subplots(figsize=(3, 3))
 
-st.header(f'Education indicator from {
-          from_year} to {to_year}', divider='gray')
+# ax.set_ylabel('Xchange Price')
+# ax.set_xlabel('Date')
+# # ax.set_xticklabels(rotation=45)
+# ax.plot(gdp_df.index, gdp_df['Value'],  label='True value')
+# ax.plot(y_pred_out, color='Red', label='SARIMA Predictions')
+# ax.legend()
 
-
-r_fig = px.line(filtered_gdp_df, x='year', y='value', color='indicator_id')
-st.plotly_chart(r_fig)
+st.header("Forcasting Haiti Xchange US Dollar/Gourdes")
 
 
-first_year = gdp_df[gdp_df['year'] == from_year]
-last_year = gdp_df[gdp_df['year'] == to_year]
+gdf_x = gdp_df.reset_index()
+gdf_x = gdf_x.sort_values('StartDate')
 
-st.header(f'Indicator description', divider='gray')
+gdf_x['StartDate'] = pd.to_datetime(gdf_x['StartDate'], format='%Y-%m-%d')
+y_pred_out = y_pred_out.reset_index()
+print(y_pred_out)
 
+fig_x = px.line(gdf_x, x=gdf_x.StartDate, y=gdf_x.Value)
 
+fig_y = go.Figure()
+
+fig_y.add_trace(go.Scatter(x=gdf_x.StartDate,
+                y=gdf_x.Value,  name='True Values', line=dict(color='red', width=4)))
+fig_y.add_trace(go.Scatter(x=y_pred_out.StartDate, y=y_pred_out.Predictions, name='Predicted Values',
+                           line=dict(color='royalblue', width=4)))
+fig_y.update_layout(title='Forcasting Haiti Xchage US Dollar/Gourdes',
+                    xaxis_title='Date',
+                    yaxis_title='Value')
+st.plotly_chart(fig_y)
 # cols = st.columns(4)
 
 # for i, country in enumerate(selected_countries):
@@ -147,10 +234,3 @@ st.header(f'Indicator description', divider='gray')
 #             delta=growth,
 #             delta_color=delta_color
 #         )
-
-
-DATA_FILENAME_2 = Path(__file__).parent/'data/sdg_indicatorlist_hti.csv'
-gdp_df_list = pd.read_csv(DATA_FILENAME_2, skiprows=[1,])
-filtered_gdp_df_list = gdp_df_list[gdp_df_list['indicator_id'].isin(
-    selected_countries)]
-st.table(filtered_gdp_df_list[['indicator_id', 'indicator_label_en']])
